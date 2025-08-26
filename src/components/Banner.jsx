b288cb9c1732fda4412a74561c024f01694f2345
unknown <sunnapusivaprasad125@gmail.com>
2025-08-26 15:04:44 +0530

    second commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Banner() {
   return (
    <div className='relative p-0 ml-[0px] mr-[0px] mt-[10px]  mt-10 place-items-center' >
-    <img className=' w-full top-0 ml-[0px] mr-[0px] mt-[10px] w-full h-[55vh] p-0' src="/img/banner2.jpg" alt="" />
+    <img className=' w-full top-0 ml-[0px] mr-[0px] mt-[10px] w-full h-[55vh] p-0' src="https://wallpapers.com/images/featured/avengers-vm16xv4a69smdauy.jpg" alt="" />
     <div className='text-white w-full text-xl bg-gray-700/60 text-center absolute bottom-0 mx-[86px]'>Avengers</div>
    </div>
     
